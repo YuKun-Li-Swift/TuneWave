e993a64d00cfa32a949c90deec2fc6c2fe28851e
YuKun-Li-Swift <wzbnzlwv@outlook.com>
2024-06-16 18:18:51 +0800

    增加了开源说明页面

---
+++ TuneWave Watch App/OpenSourcePage.swift
@@ -0,0 +1,18 @@
+//
+//  OpenSourcePage.swift
+//  TuneWave Watch App
+//
+//  Created by Yukun Li on 2024/6/16.
+//
+
+import SwiftUI
+
+struct OpenSourcePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpenSourcePage()
+}
